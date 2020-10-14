b620f81a9c7d091ee535a4437d60c47e33ad1873
Yannis Biasutti <yannis@biasutti.ch>
2020-10-14 22:03:06 +0200

    Add spring-context dependency and add given classes

---
+++ src/main/java/ch/bfh/obelix/heroes/service/HeroService.java
@@ -0,0 +1,8 @@
+package ch.bfh.obelix.heroes.service;
+
+import ch.bfh.obelix.heroes.model.Hero;
+
+public interface HeroService {
+
+    Hero createHero(String name);
+}
---
+++ src/main/java/ch/bfh/obelix/heroes/service/PartyService.java
@@ -0,0 +1,8 @@
+package ch.bfh.obelix.heroes.service;
+
+import ch.bfh.obelix.heroes.model.Party;
+
+public interface PartyService {
+
+    Party createParty(String name);
+}
